37a30dfb8524023ec4c6413d4c3cf88b02b0c5e8
398467 <398467@H0593>
2020-09-14 12:00:51 +0100

    first changes

--- Frameworkapp/Frameworkapp/Program.cs
+++ Frameworkapp/Frameworkapp/Program.cs
@@ -10,6 +10,21 @@ namespace Frameworkapp
     {
         static void Main(string[] args)
         {
+
+            short num1, num2;
+            int addAns;
+            //num1 = 3;
+            //num2 = 2;
+            Console.WriteLine("Enter Number: ");
+            num1 = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Number: ");
+            num2 = short.Parse(Console.ReadLine());
+
+            addAns = num1 + num2;
+            Console.WriteLine(num1 + " + " + num2 + " is equal to " + addAns);
+
+            Console.Read();
         }
     }
 }
